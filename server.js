778a2a12f142c060c234d7321a633c59e8f83461
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-02-13 07:42:37 -0600

    connect mysql database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// connect mysql2 from node_modules
+const mysql = require('mysql2')
 const express = require('express');
 const PORT = process.env.PORT ||3001;
 const app = express();
@@ -5,6 +7,18 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+//connect application to the MySQL database
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
 //Default response for any other request (Not found)
 app.use((req, res) =>{
